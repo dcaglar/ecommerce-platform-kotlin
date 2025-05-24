f9ca0577f881ee51dc4ad49f1eacf4d167da47cf
dcaglar <dcaglar1987@gmail.com>
2025-05-24 04:27:42 +0200

    add add uniquness redis ontainer test

--- payment-service/src/main/kotlin/com/dogancaglar/paymentservice/domain/port/IdGenerator.kt
+++
@@ -1,8 +0,0 @@
-package com.dogancaglar.paymentservice.domain.port
-
-interface IdGenerator {
-    fun nextId(namespace: String): Long
-    fun nextPublicId(prefix: String): String
-    fun getRawValue(namespace: String): Long?
-    fun setMinValue(namespace: String, value: Long)
-}
