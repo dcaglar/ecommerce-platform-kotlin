1e26ea8c6c991189f12be55a1a7d36197d733e72
dogancaglar <dcaglar1987@gmail.com>
2026-06-30 18:12:39 +0200

    fix again

--- keycloak/get-token-azure.sh
+++ keycloak/get-token-azure.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-KEYCLOAK_IP=$(kubectl get svc -n payment keycloak -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+#KEYCLOAK_IP=$(kubectl get svc -n payment keycloak -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export KEYCLOAK_IP=20.23.200.203
 KEYCLOAK_URL="http://${KEYCLOAK_IP}:8080"
 bash "$(dirname "$0")/get-token.sh" "$KEYCLOAK_URL" "$@"
